ec5d7e93311e79643657766fe2b62137d7d5031f
Leonardo <leonardofallardi@gmail.com>
2019-12-28 13:15:12 -0300

    cambios en el welcome

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,124 +1,7 @@
 import React, { Component, useState, useEffect } from 'react'
-import { Modal, StyleSheet, Text, TextInput, View, Keyboard, TouchableOpacity, FlatList, TouchableWithoutFeedback, Alert } from 'react-native'
+import { Modal, StyleSheet, Text, Image, TextInput, View, Keyboard, TouchableOpacity, FlatList, TouchableWithoutFeedback, Alert, Dimensions } from 'react-native'
 import * as firebase from "firebase";
-
-// const dtResto = [
-//   {
-//     id: '1',
-//     title: 'Boulangerie',
-//     WaitTime: 'none'
-//   },
-//   {
-//     id: '2',
-//     title: 'Caribeño',
-//     WaitTime: 'none'
-//   },
-//   {
-//     id: '3',
-//     title: 'Cielito Lindo',
-//     WaitTime: 'none'
-//   },
-//   {
-//     id: '4',
-//     title: 'Cusco',
-//     WaitTime: 'none'
-//   },
-//   {
-//     id: '5',
-//     title: 'Habibi',
-//     WaitTime: 'none'
-//   },
-//   {
-//     id: '6',
-//     title: 'Jade',
-//     WaitTime: 'none'
-//   },
-//   {
-//     id: '7',
-//     title: 'JC Stakehouse',
-//     WaitTime: 'none'
-//   },
-//   {
-//     id: '8',
-//     title: 'La Cantina by Cielito Lindo',
-//     WaitTime: 'none'
-//   },
-//   {
-//     id: '9',
-//     title: 'Le Chateau',
-//     WaitTime: 'none'
-//   },
-//   {
-//     id: '10',
-//     title: 'Lobby Bar',
-//     WaitTime: 'none'
-//   },
-//   {
-//     id: '11',
-//     title: 'Los Tacos',
-//     WaitTime: 'none'
-//   },
-//   {
-//     id: '12',
-//     title: 'Lounge Bar Caribeño Terrace',
-//     WaitTime: 'none'
-//   },
-//   {
-//     id: '13',
-//     title: 'Lounge Bar Cusco Terrace',
-//     WaitTime: 'none'
-//   },
-//   {
-//     id: '14',
-//     title: 'Snack La Selva',
-//     WaitTime: 'none'
-//   },
-//   {
-//     id: '15',
-//     title: 'Swim Up Bar Caribeño',
-//     WaitTime: 'none'
-//   },
-//   {
-//     id: '16',
-//     title: 'Swim Up Bar Cusco',
-//     WaitTime: 'none'
-//   },
-//   {
-//     id: '17',
-//     title: 'Tavola',
-//     WaitTime: 'none'
-//   },
-//   {
-//     id: '18',
-//     title: 'Tapas y Vinos',
-//     WaitTime: 'none'
-//   },
-//   {
-//     id: '19',
-//     title: 'The Grand Buffet',
-//     WaitTime: 'none'
-//   },
-//   {
-//     id: '20',
-//     title: 'The Grill Snack Bar',
-//     WaitTime: 'none'
-//   },
-//   {
-//     id: '21',
-//     title: 'The Grill Swim Up Bar',
-//     WaitTime: 'none'
-//   },
-//   {
-//     id: '22',
-//     title: 'The Ninth Pin Bowlin & Bar',
-//     WaitTime: 'none'
-//   },
-//   {
-//     id: '23',
-//     title: 'The Library Speakeasy Bar',
-//     WaitTime: 'none'
-//   },
-// ];
+import Carousel from 'react-native-snap-carousel';
 
 const WelcomeScreen=({ navigation }) => {
   const [NameResto, setNameResto] = useState("");
@@ -138,20 +21,6 @@ const WelcomeScreen=({ navigation }) => {
             ...values,
             [change.doc.id]: change.doc.data()
           }));
-
-          // objeto que se genera en tareas
-          // {
-          //   "zG9CgjoHvKTqIiBWZpf8": {
-          //     titulo: "",
-          //     fecha: "",
-          //     done: false
-          //   },
-          //   "zG9CgjoHvKTtIiBWZpf8": {
-          //     titulo: "",
-          //     fecha: "",
-          //     done: false
-          //   }
-          // }
         }
         if (change.type === "removed") {
           setResto(values => {
@@ -184,20 +53,67 @@ const WelcomeScreen=({ navigation }) => {
       Date: firebase.firestore.FieldValue.serverTimestamp()
     });
   };
+  
+  const renderItem = (Name) => {
+    console.log(Name, 'entre');
+    return (
+      <View style={styles.Card}>
+          <Text style={styles.title}>{ Name }</Text>
+      </View>
+    );
+  };
+
+
 
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
       <View style={styles.container}>
-        <Text style={styles.UserName}>{navigation.state.params.Name.substring(0, 10) + ' ' + navigation.state.params.LastName.substring(0, 10)}</Text>
-        <Text style={styles.UserNameInitial}>{navigation.state.params.Name.substring(0, 1) + navigation.state.params.LastName.substring(0, 1)}</Text>
-        <TextInput key="Search" 
+        {/* <Text style={styles.UserName}>{navigation.state.params.Name.substring(0, 10) + ' ' + navigation.state.params.LastName.substring(0, 10)}</Text>
+        <Text style={styles.UserNameInitial}>{navigation.state.params.Name.substring(0, 1) + navigation.state.params.LastName.substring(0, 1)}</Text> */}
+        {/* <TextInput key="Search" 
                    style={styles.Buscador} 
                    placeholder="Type here the name of resto..." 
                    value={Search} 
                    onChangeText={(text) => setSearch(text)} 
-                   onKeyPress={BuscarResto}/>
+                  onKeyPress={BuscarResto}/> */}
 
-        <FlatList
+        <Carousel
+              // ref={(c) => { carousel = c; }}
+              data={Object.keys(Restos)}
+              renderItem={( {item:key} ) => {return <View style={styles.Card} title={Restos[key].id}>
+                                                      <Image source={{uri:Restos[key].img? Restos[key].img: ''}} style={styles.stretch}/>    
+                                                      <Text style={styles.Title}> {Restos[key].subtitle} </Text>
+                                                      <View style={styles.contenedorAcciones}>
+                                                        <TouchableOpacity
+                                                          style={[styles.btnReserve]}
+                                                          onPress={() => {
+                                                            setNameResto(Restos[key].Name);
+                                                            setIdResto(Restos[key].ID);
+                                                            reservarResto();
+                                                          }}
+                                                          >
+                                                          <Text style={color='#FFF'}>Reservar</Text>
+                                                        </TouchableOpacity>
+
+                                                        <TouchableOpacity
+                                                          style={[styles.btnReserve]}
+                                                          onPress={() => {
+                                                            console.log(Restos[key], 'objeto');
+                                                            console.log(Restos[key].img, 'la imagen');
+                                                            console.log(Restos[key].Name, 'nombre');
+                                                            console.log(Restos[key].ID, 'el id');
+                                                          }}
+                                                        >
+                                                          <Text style={color='#FFF'}>Calificar</Text>
+                                                        </TouchableOpacity>
+                                                      </View>
+                                                    </View>}
+              }
+              sliderWidth={Dimensions.get('window').width}
+              itemWidth={300}
+        />
+
+        {/* <FlatList
           data={Object.keys(Restos).filter(key => Restos[key].Name.includes(Search))}
           renderItem={( {item:key} ) => {return <View title={Restos[key].id}>
                                                   <TouchableOpacity
@@ -213,9 +129,8 @@ const WelcomeScreen=({ navigation }) => {
                                                   </TouchableOpacity>
                                                 </View>}}
           keyExtractor={key => Restos[key].id}
-        />
-
-
+        /> 
+        
         <Modal
           animationType="slide"
           transparent={true}
@@ -245,7 +160,7 @@ const WelcomeScreen=({ navigation }) => {
 
               </View>
             </View>
-        </Modal>
+        </Modal> */}
 
       </View>
     </TouchableWithoutFeedback>
@@ -256,58 +171,37 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     width: 400,
-    backgroundColor: '#FFF',
+    backgroundColor: '#000',
     justifyContent: 'flex-start',
     paddingTop: 30,
     paddingLeft: 10,
     paddingRight: 10,
   },
   
-  UserName: {
-    width: 210,
-    height: 50,
-    borderRadius: 25,
-    backgroundColor: '#D3D3D3',
-    color: '#FFF',
-    fontSize: 14,
-    textAlign: 'center',
-    paddingTop: 15,
-    paddingLeft: 25,
-  }, 
-  
-  UserNameInitial: {
-    width: 50,
-    height: 50,
-    borderRadius: 25,
-    backgroundColor: '#259D97',
-    color: '#FFF',
-    fontSize: 20,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    paddingTop: 10,
-    marginTop: -50,
-  },
-  
-  Buscador: {
-    margin: 15,
+  stretch: {
     width: 300,
-    marginBottom: 10,
-    borderBottomWidth: 1,
-    borderBottomColor: '#259D97',
-    justifyContent: 'center',
-  },
-
-  item: {
-    flex: 1,
-    flexDirection: 'row',
-    backgroundColor: '#FFF',
-    padding: 10,
-    width: 300,
+    resizeMode: 'contain',
   },
 
   Title: {
     fontSize: 16,
-    width: 200
+    color: '#A9733E',
+  },
+
+  btnReserve: {
+    flex: 1,
+    borderRadius: 25,
+    backgroundColor: '#A9733E',
+    padding: 10,
+    alignItems: 'center',
+
+  },
+  
+  contenedorAcciones: {
+    flexDirection: 'row',
+    flex: 1,
+    justifyContent: 'space-around',
+    alignItems: 'flex-end',
   },
 
   Time: {
@@ -315,25 +209,85 @@ const styles = StyleSheet.create({
     fontSize: 16,
     width: 100
   },
-
-  Modal: {
+  
+  Card: {
     height: 500,
-    width: 340,
+    borderRadius: 25,
     marginLeft: 10,
     marginRight: 10,
-    backgroundColor: '#FFF',
+    backgroundColor: '#161616',
     padding: 10,
     shadowColor: '#A9733E',
     shadowOffset: {
-	    width: 0,
-	    height: 3,
+      width: 0,
+      height: 3,
     },
     shadowOpacity: 0.29,
     shadowRadius: 4.65,
-
+    
     elevation: 7,
   }
-
+  
 })
 
 export default WelcomeScreen;
+// Modal: {
+  //   height: 500,
+  //   width: 340,
+//   marginLeft: 10,
+//   marginRight: 10,
+//   backgroundColor: '#FFF',
+//   padding: 10,
+//   shadowColor: '#A9733E',
+//   shadowOffset: {
+  //     width: 0,
+  //     height: 3,
+  //   },
+  //   shadowOpacity: 0.29,
+//   shadowRadius: 4.65,
+
+//   elevation: 7,
+// },
+
+// item: {
+//   flex: 1,
+//   flexDirection: 'row',
+//   backgroundColor: '#FFF',
+//   padding: 10,
+//   width: 300,
+// },
+
+// UserName: {
+  //   width: 210,
+//   height: 50,
+//   borderRadius: 25,
+//   backgroundColor: '#D3D3D3',
+//   color: '#FFF',
+//   fontSize: 14,
+//   textAlign: 'center',
+//   paddingTop: 15,
+//   paddingLeft: 25,
+// }, 
+
+// Buscador: {
+//   margin: 15,
+//   width: 300,
+//   marginBottom: 10,
+//   borderBottomWidth: 1,
+//   borderBottomColor: '#259D97',
+//   justifyContent: 'center',
+// },
+
+  
+// UserNameInitial: {
+//   width: 50,
+//   height: 50,
+//   borderRadius: 25,
+//   backgroundColor: '#259D97',
+//   color: '#FFF',
+//   fontSize: 20,
+//   fontWeight: 'bold',
+//   textAlign: 'center',
+//   paddingTop: 10,
+//   marginTop: -50,
+// },
